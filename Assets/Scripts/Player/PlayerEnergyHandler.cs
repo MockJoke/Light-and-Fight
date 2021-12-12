b661714c91f6e4dbf42198fe7e088ddaffb4f16b
P∧R∨A <parvadesai404@gmail.com>
2021-12-12 17:00:34 +0530

    Created Energy Handlers

---
+++ Assets/Scripts/Player/PlayerEnergyHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class PlayerEnergyHandler : MonoBehaviour
+{
+    public Image energyBar;
+    public GameObject gameoverScreen; 
+    public CollisionDetectionHandler collisionDetectionHandler;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
